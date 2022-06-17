9b6a383c0691be56825acb1695203247a18f421c
HullQin <hullqin@outlook.com>
2022-06-17 16:21:40 +0800

    界面响应式设计

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,30 @@
 import { Loader } from '@pixi/loaders';
-import { Fruits } from './config';
+import { Fruits, Height, Width } from './config';
 import app from './app';
 import { init } from './core';
 import './index.css';
 
 const images = Fruits.map((i) => i.name);
-document.getElementById('root')!.appendChild(app.view);
+const root = document.getElementById('root')!;
+const canvas = app.view;
+root.appendChild(canvas);
 Loader.shared.add(images).load(init);
+
+const resetSize = () => {
+  canvas.style.width = `${Width}px`;
+  canvas.style.height = `${Height}px`;
+  const { innerWidth, innerHeight } = window;
+  if (innerWidth / innerHeight > Width / Height) {
+    root.style.height = `${innerHeight}px`;
+    root.style.width = `${innerHeight / Height * Width}px`;
+    canvas.style.transform = `scale(${innerHeight / Height})`;
+  } else {
+    root.style.width = `${innerWidth}px`;
+    root.style.height = `${innerWidth / Width * Height}px`;
+    canvas.style.transform = `scale(${innerWidth / Width})`;
+  }
+};
+
+resetSize();
+
+window.onresize = resetSize;
